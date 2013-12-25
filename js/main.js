3806d91f2846d0c259ee1b03d9e2f421191d7203
Megh Vakharia <meghhv@gmail.com>
2013-12-25 12:09:05 -0800

    Added improvements to sliding, including responsive time change

--- js/main.js
+++ js/main.js
@@ -1,32 +1,51 @@
-$(document).ready(function() { 
-	$(document.body).fadeIn(2000);
-	resizeDiv($("#home"));
-	resizeDiv($("#about"));
-	resizeDiv($("#projects"));
-	resizeDiv($("#contact"));
-	$("#banner a").bind("click",function(event){ 
-		event.preventDefault(); 
-		var target = $(this).attr("href"); 
-		$("html, body").stop().animate({ 
-			scrollLeft: $(target).offset().left, 
-			scrollTop: $(target).offset().top 
-		}, 1200); 
+(function(window){
+	var target;
+
+	$(document).ready(function() { 
+		$(document.body).fadeIn(2000);
+		resizeDiv($("#home"));
+		resizeDiv($("#about"));
+		resizeDiv($("#projects"));
+		resizeDiv($("#contact"));
+		$("#banner a").bind("click",function(event){ 
+			event.preventDefault(); 
+			target = $(this).attr("href"); 
+			slide(setSlideTime);
 			$("body").css("background-color", "green");
+		}); 
 	}); 
-}); 
 
-window.onresize = function(event) {
-	resizeDiv($("#home"));
-	resizeDiv($("#about"));
-	resizeDiv($("#projects"));
-	resizeDiv($("#contact"));}
+	this.onresize = function(event) {
+		resizeDiv($("#home"));
+		resizeDiv($("#about"));
+		resizeDiv($("#projects"));
+		resizeDiv($("#contact"));
+		slide(0);	}
 
-function resizeDiv(div) {
-	vpw = $(window).width();
-	vph = $(window).height();
-	$(div).css({
-		"height": vph + "px",
-		"width": vpw + "px"
-	});
-}
+	function resizeDiv(div) {
+		vpw = $(this).width();
+		vph = $(this).height();
+		$(div).css({
+			"height": vph + "px",
+			"width": vpw + "px"
+		});
+	}
 
+	function slide(time) {
+		$("html, body").stop().animate({ 
+				scrollLeft: $(target).offset().left, 
+				scrollTop: $(target).offset().top 
+		}, time);
+	}
+
+	function setSlideTime() {
+		var time;
+		if($(this).width() > 768) {
+			time = 1200;
+		} else {
+			time = 800;
+		}
+		console.log(time);
+		return time;
+	}
+})(this);
